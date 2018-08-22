09b8b0ab843f71cf64a41e588f9f9467ab7e9230
nanyash <miss.nanya80@gmail.com>
2018-08-22 12:39:39 +0300

    initial

--- src/main/java/com/ItemController.java
+++ src/main/java/com/ItemController.java
@@ -1,46 +1,56 @@
 package com;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @Controller
 public class ItemController {
 
     @Autowired
     private ItemService itemService;
+    private ObjectMapper mapper = new ObjectMapper();
 
     @RequestMapping(method = RequestMethod.POST, value = "/save", produces = "text/plain")
     @ResponseBody
-    public String save(Item item) throws InternalServerError {
+    public String save(HttpServletRequest req) throws InternalServerError {
         String message;
         try {
-            message = "Item :"+itemService.save(item)+" is saved.";
+            message = "Item :"+itemService.save(mapToItem(req))+" is saved.";
             return message;
         }catch (InternalServerError e){
-            return "InternalServerError";
+            e.printStackTrace();
+            return e.getMessage();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new InternalServerError("Something wrong").getMessage();
         }
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "/delete", produces = "text/plain")
+    @RequestMapping(method = RequestMethod.PUT, value = "/update", produces = "text/plain")
     @ResponseBody
-    public String update(Item item) throws InternalServerError, BadRequestException {
+    public String update(HttpServletRequest req) throws InternalServerError, BadRequestException{
         String message;
         try {
-            message = "Item :"+itemService.update(item).toString()+" is udated.";
+            message = "Item :"+itemService.update(mapToItem(req)).toString()+" is updated.";
             return message;
         }catch (BadRequestException | InternalServerError e){
             return e.getMessage();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new InternalServerError("Something wrong").getMessage();
         }
 
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/delete", produces = "text/plain")
     @ResponseBody
-    public String delete(Long id) throws InternalServerError, BadRequestException {
+    public String delete(@RequestParam Long id) throws InternalServerError, BadRequestException {
         String message;
         try {
             itemService.delete(id);
@@ -62,6 +72,14 @@ public class ItemController {
             return e.getMessage();
         }
     }
+
+    private Item mapToItem(HttpServletRequest req) throws IOException {
+        JsonParser jsonParser = mapper.getFactory().createParser(req.getInputStream());
+        Item item = mapper.readValue(jsonParser, Item.class);
+        System.out.println(item);
+        return item;
+    }
+
 
 
 }
